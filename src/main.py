19c9a3114a3fca48a2332fcd8f180cd628100527
48lexR <cbull358@gmail.com>
2025-04-05 22:38:02 -0400

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -3,17 +3,16 @@ from tensorflow import keras
 from model import readData
 import numpy as np
 
-TEAMS = []
-
 TEAMS = readData()
 
 def main():
     teams = TEAMS
     model = keras.models.load_model("./src/mmpredictor.keras")
-    team1 = input("Input team name:")
-    team2 = input("Input team name:")
-    outcome = tf.constant(np.asarray([[teams[team1].asList(), teams[team2].asList()]]), dtype="float32")
-    print(f"{team1}: {int(model.predict(outcome)[0][0])} - {team2}: {int(model.predict(outcome)[0][1])}")
+    team1 = input("Input team name: ")
+    team2 = input("Input team name: ")
+    match = tf.constant(np.asarray([[teams[team1].asList(), teams[team2].asList()]]), dtype="float32")
+    outcome = model.predict(match)
+    print(f"{team1}: {int(outcome[0][0])} - {team2}: {int(outcome[0][1])}")
 
 
 if __name__ == "__main__":
